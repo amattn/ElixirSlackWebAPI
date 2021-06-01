40c5d0478a87dbe21ab945cd1b0904c1af29f7aa
amattn <amattn@users.noreply.github.com>
2021-05-31 17:04:20 -0700

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SlackWeb.Mixfile do
   def project do
     [
       app: :slack_web,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: ">= 1.9.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       name: "SlackWeb",
